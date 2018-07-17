40163784b82b7590c387f88799daa4ddd52fe7ec
gaoz <nnugaozhi@hotmail.com>
2018-07-17 17:36:44 +0800

    create product entity

---
+++ SportsStore2.Domain/Concrete/EFDbContext.cs
@@ -0,0 +1,17 @@
+﻿using SportsStore2.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SportsStore2.Domain.Concrete
+{
+    public class EFDbContext : DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+
+
+    }
+}
---
+++ SportsStore2.Domain/Concrete/EFProductRepository.cs
@@ -0,0 +1,23 @@
+﻿using SportsStore2.Domain.Abstract;
+using SportsStore2.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SportsStore2.Domain.Concrete
+{
+    public class EFProductRepository : IProductRepository
+    {
+        EFDbContext context = new EFDbContext();
+
+        public IEnumerable<Product> Products
+        {
+            get
+            {
+                return context.Products;
+            }
+        }
+    }
+}
